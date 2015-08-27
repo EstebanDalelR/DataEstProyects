5ceddb81974888c23cf90876c7b5b0e676566ea0
Esteban Dalel R <estebandalelr@gmail.com>
2015-08-27 08:26:08 -0500

    Agregar clases a CentralLlamads

--- pry_estructuras/pry_estructuras/esqueleto-estructuras/src/pry_estructuras/Lista.java
+++ pry_estructuras/pry_estructuras/esqueleto-estructuras/src/pry_estructuras/Lista.java
@@ -3,8 +3,6 @@ package pry_estructuras;
 import estructuras.lista.ILista;
 import java.util.Iterator;
 
-public class Lista<T> implements ILista<T>
+public class Lista<Caso> implements ILista<Caso>
 {
-  
-
 }
