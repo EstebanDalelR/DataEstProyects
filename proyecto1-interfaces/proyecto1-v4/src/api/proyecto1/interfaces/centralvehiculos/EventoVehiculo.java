8431b465ecc287bc26923f85652339ff3df9e2ac
Esteban Dalel R <estebandalelr@gmail.com>
2015-08-27 13:53:16 -0500

    Reconstruir archivos

---
+++ proyecto1-interfaces/proyecto1-v4/src/api/proyecto1/interfaces/centralvehiculos/EventoVehiculo.java
@@ -0,0 +1,38 @@
+package api.proyecto1.interfaces.centralvehiculos;
+public class EventoVehiculo implements IEventoVehiculo
+{
+
+  private TipoEventoVehiculo tipoEvento;
+//public final class Long fecha;
+   public enum TipoEventoVehiculo;
+  public void setTipo(TipoEventoControl tipo)
+  {
+  tipoEvento = tipo;
+  }
+   /**
+  * M�todo que retorna el tipo del evento
+  * @return TipoEvento tipo del evento
+  */
+  public TipoEventoControl getTipo()
+  {
+    return tipoEvento;
+  }
+
+ /**
+  * Método que retorna la estampilla de tiempo del evento como un long de milisegundos
+  * @return retorna la estampilla de tiempo como long de milisegundos
+  */
+  public long getEstampillaTiempo(){
+  return fecha;
+  }
+
+ /**
+  * M�todo que permite establecer la estampilla de tiempo para el evento como un long de milisegundos
+  * @param estampillaTiempo estampilla de tiempo del evento como un long de milisegundos
+  */
+  public void setEstampillaTiempo(long estampillaTiempo){
+fecha=estampillaTiempo;
+  }
+}
+  
+}
