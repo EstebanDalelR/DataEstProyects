d5a2b8a8e15da59a2ba0a562c0159e6560fb739c
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2015-09-06 17:13:13 -0500

    nombre

---
+++ EntregaFinalDomingo/proyecto1-v4/src/api/proyecto1/interfaces/centralvehiculos/TipoVehiculo.java
@@ -0,0 +1,10 @@
+package api.proyecto1.interfaces.centralvehiculos;
+
+/*
+ * TipoVehiculo se refiere a los tipos de vehiculo que pueden recibir la central de vehiculo
+ */
+public enum TipoVehiculo{
+CAMION,
+FURGON,
+
+}
