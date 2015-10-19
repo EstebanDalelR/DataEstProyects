7b8c9a38eee9d82c32e0f5be5c007ef7d04ff689
Esteban Dalel R <estebandalelr@gmail.com>
2015-10-18 23:18:25 -0500

    Hacer ParteA.java

--- Proyecto2/Proyecto2151015/src/mundo/ParteA.java
+++ Proyecto2/Proyecto2151015/src/mundo/ParteA.java
@@ -5,7 +5,7 @@ public class ParteA {
     public void executeParteATest() {
 
         /**
-        * TODO Instanciar una Central de Vehiculos e inicializarla con los datos del archivo formato JSON de la version mas completa y correcta disponible.
+        *  Instanciar una Central de Vehiculos e inicializarla con los datos del archivo formato JSON de la version mas completa y correcta disponible.
          */
          CentralVehiculos NCV = new CentralVehiculos("/data/JSON");
 
@@ -20,12 +20,19 @@ public class ParteA {
          long tTotalConsultaClientes = System.nanoTime() - tInicioConsultaClientes; 
         System.out.println("tiempo Consulta Clientes: " + tTotalConsultaClientes + " nseg");
 
-
-        /**TODO Mostrar el nombre, apellido, identificador y la prioridad de cada cliente */
+        public void darDatosClientes()
+        {
+            for (i=0; i<darListaClientes;  i++ )
+            {
+                System.out.println(darListaClientes[i].darDatos());    
+            }
+        }
+        /** Mostrar el nombre, apellido, identificador y la prioridad de cada cliente */
 
 
         long tInicioRuta = System.nanoTime();
-        /**TODO Requerimiento 2: Utilizar la prioridad de los clientes para definir una ruta con las paradas autorizadas de recogida y/o de entrega de pedidos/despachos a los clientes.
+        /**TODO Requerimiento 2: Utilizar la prioridad de los clientes para definir una ruta con las paradas autorizadas de recogida y/o de 
+        *entrega de pedidos/despachos a los clientes.
          * Se obtiene una ruta tal que se asigna las paradas programadas de sus clientes en orden descendente de prioridad. 
         * Recibe la informacion necesaria de la ruta para actualizarla segun la prioridad de sus clientes.
          */
@@ -33,7 +40,8 @@ public class ParteA {
         System.out.println("tiempo creacion ruta: " + tTotalRuta + " nseg");
 
 
-        /**TODO Mostrar la informacion de paradas autorizadas de la ruta. Por cada para parada mostrar: Id pedido/despacho, id cliente, prioridad del cliente, punto. La informacion se muestra en el orden de recorrido de la ruta. */
+        /**TODO Mostrar la informacion de paradas autorizadas de la ruta. Por cada para parada mostrar: Id pedido/despacho, id cliente, 
+        *prioridad del cliente, punto. La informacion se muestra en el orden de recorrido de la ruta. */
 
 
         long tInicioBusquedaNumPedido = System.nanoTime();
@@ -41,6 +49,15 @@ public class ParteA {
          * Se obtiene el numero de despacho de un pedido/despacho si en la fecha indicada se presto un servicio
          * al cliente indicado. 
          */
+        public int buscarPedidoFecha(Date aBuscar, int idCliente)
+        {
+            for (i=0; i<NCV.arbolFecha.tamano();i++ ) {
+                if (NCV.arbolFecha.contieneLlave(aBuscar)) {
+                    return NCV.arbolFecha.get(aBuscar, idCliente);
+                }
+                return -1;
+            }
+        }
         long tTotalBusquedaNumPedido = System.nanoTime() - tInicioBusquedaNumPedido; 
         System.out.println("tiempo busqueda numero de pedido: " + tTotalBusquedaNumPedido + " nseg");
 
@@ -55,6 +72,15 @@ public class ParteA {
          * de la recepcion o entrega, nombre del funcionario de CCT que atendio al cliente y
          * observaciones o anotaciones.
          */
+        public Pedido buscarPedidoFecha(int idPedido)
+        {
+            for (i=0; i<NCV.hashInfo.darTamano();i++ ) {
+                if (NCV.hashInfo.obtener(idCliente)!= null) {
+                    return NCV.hashInfo.obtener( idCliente);
+                }
+                return null;
+            }
+        }
         long tTotalBusquedaInfoPedido = System.nanoTime() - tInicioBusquedaInfoPedido; 
         System.out.println("tiempo busqueda informacion de pedido: " + tTotalBusquedaInfoPedido + " nseg");
 
