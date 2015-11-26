7c8671bc40d444a4ae7d851de0571bf86b796c27
EstebanDalelR <estebandalelr@gmail.com>
2015-11-25 22:49:00 -0500

    implementar parte A, indentar Dijkstra

--- Proyecto3/Proyecto3_v1/src/estructuras/Dijkstra.java
+++ Proyecto3/Proyecto3_v1/src/estructuras/Dijkstra.java
@@ -6,29 +6,29 @@ public class Dijkstra {
 	//listas de respuesta
 	private Lista<Double> distTo;   // distTo[v] = distance  of shortest s->v path
 	private Lista<Arco> edgeTo;    // lista de arcos que conectan los dos v√©rtices
-	
+
 	public Dijkstra(){
 		distTo=new Lista<>();
 		edgeTo=new Lista<>();
 	}
 	public static Comparator<Arco> menorTiempo =new Comparator<Arco>() 
-			{
-			@Override
-			//compara las distancias entre dos arcos
-			public int compare(Arco o1, Arco o2) {
-				Double dist1= (Double)o1.getValor();
-				Double dist2=(Double)o2.getValor();
-				Double vel1=(Double)o1.getValor2();
-				Double vel2=(Double)o2.getValor2();
-				Double t1=(Double)dist1/vel1;
-				Double t2=(Double)dist2/vel2;
-				Double respuesta=(t1)-(t2);
-				int resp= (int)((double)respuesta) ;
-				return resp;
-			}		
-		};
+	{
+		@Override
+		//compara las distancias entre dos arcos
+		public int compare(Arco o1, Arco o2) {
+			Double dist1= (Double)o1.getValor();
+			Double dist2=(Double)o2.getValor();
+			Double vel1=(Double)o1.getValor2();
+			Double vel2=(Double)o2.getValor2();
+			Double t1=(Double)dist1/vel1;
+			Double t2=(Double)dist2/vel2;
+			Double respuesta=(t1)-(t2);
+			int resp= (int)((double)respuesta) ;
+			return resp;
+		}		
+	};
 	public static Comparator<Arco> menorDistancia =new Comparator<Arco>() {
-		
+
 		@Override
 		//compara las distancias entre dos arcos
 		public int compare(Arco o1, Arco o2) {
@@ -56,86 +56,86 @@ public class Dijkstra {
 			return resp;
 		}		
 	};
-	
+
 	public Lista<Arco> dijkstraTiempo(Vertice origen,Vertice meta){
-		
-	
+
+
 		//produce una lista con los arcos de menor distancia entre dos ejes
 		Lista<Arco> respuesta = new Lista<Arco>();
 
 		//crea un queue con los arcos a explorar por los que demoran menos tiempo
-			PriorityQueue<Arco> queueSiguientes = 
-					new PriorityQueue<Arco>(origen.getArco().darTamaÒo(), menorTiempo);
-			while(!queueSiguientes.isEmpty()){
-				//toma el v√©rtice de ese primer arco
-				Arco actual=queueSiguientes.poll();
-				Vertice destActual= actual.getDestino();
-				//si es la meta, retorna
-				respuesta.agregar(actual);
-				
-				if (destActual.equals(meta)) {
-					return respuesta;
-				}
-				Lista<Arco> agregar = destActual.getArco();
-				Collections.sort(agregar, menorTiempo);
-				queueSiguientes.addAll((Collection<? extends Arco>) agregar);
-				
-				//http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
+		PriorityQueue<Arco> queueSiguientes = 
+				new PriorityQueue<Arco>(origen.getArco().darTamaÒo(), menorTiempo);
+		while(!queueSiguientes.isEmpty()){
+			//toma el v√©rtice de ese primer arco
+			Arco actual=queueSiguientes.poll();
+			Vertice destActual= actual.getDestino();
+			//si es la meta, retorna
+			respuesta.agregar(actual);
+
+			if (destActual.equals(meta)) {
+				return respuesta;
 			}
-			return  respuesta;
+			Lista<Arco> agregar = destActual.getArco();
+			Collections.sort(agregar, menorTiempo);
+			queueSiguientes.addAll((Collection<? extends Arco>) agregar);
+
+			//http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
 		}
+		return  respuesta;
+	}
 	public Lista<Arco> dijkstra(Vertice origen,Vertice meta,Comparator<Arco> comparador){
-		
+
 		//produce una lista con los arcos de menor distancia entre dos ejes
 		Lista<Arco> respuesta = new Lista<Arco>();
 
 		//crea un queue con los arcos a explorar por los que demoran menos tiempo
-			PriorityQueue<Arco> queueSiguientes = 
-					new PriorityQueue<Arco>(origen.getArco().darTamaÒo(), comparador);
-			while(!queueSiguientes.isEmpty()){
-				//toma el v√©rtice de ese primer arco
-				Arco actual=queueSiguientes.poll();
-				Vertice destActual= actual.getDestino();
-				//si es la meta, retorna
-				respuesta.agregar(actual);
-				
-				if (destActual.equals(meta)) {
-					return respuesta;
-				}
-				Lista<Arco> agregar = destActual.getArco();
-				Collections.sort(agregar, menorTiempo);
-				queueSiguientes.addAll((Collection<? extends Arco>) agregar);
-				
-				//http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
+		PriorityQueue<Arco> queueSiguientes = 
+				new PriorityQueue<Arco>(origen.getArco().darTamaÒo(), comparador);
+		while(!queueSiguientes.isEmpty()){
+			//toma el v√©rtice de ese primer arco
+			Arco actual=queueSiguientes.poll();
+			Vertice destActual= actual.getDestino();
+			//si es la meta, retorna
+			respuesta.agregar(actual);
+
+			if (destActual.equals(meta)) {
+				return respuesta;
 			}
-			return  respuesta;
+			Lista<Arco> agregar = destActual.getArco();
+			Collections.sort(agregar, menorTiempo);
+			queueSiguientes.addAll((Collection<? extends Arco>) agregar);
+
+			//http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
 		}
+		return  respuesta;
+	}
 	public Lista<Arco> dijkstraDistancia(Vertice origen,Vertice meta){
-			//TODO
-			PriorityQueue<Arco> queueSiguientes = new PriorityQueue<>();
-			
+		//TODO
+		PriorityQueue<Arco> queueSiguientes = new PriorityQueue<>();
+
 		//produce una lista con los arcos de menor distancia entre dos ejes
 		Lista<Arco> respuesta = new Lista<Arco>();
 
 		//crea un queue con los arcos a explorar por los que demoran menos tiempo
-			queueSiguientes = 
-					new PriorityQueue<Arco>(origen.getArco().darTamaÒo(), menorTiempo);
-			while(!queueSiguientes.isEmpty()){
-				//toma el v√©rtice de ese primer arco
-				Arco actual=queueSiguientes.poll();
-				Vertice destActual= actual.getDestino();
-				//si es la meta, retorna
-				respuesta.agregar(actual);
-				
-				if (destActual.equals(meta)) {
-					return respuesta;
-				}
-				Lista<Arco> agregar = destActual.getArco();
-				Collections.sort(agregar, menorTiempo);
-				queueSiguientes.addAll((Collection<? extends Arco>) agregar);
-				
-				//http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
+		queueSiguientes = 
+				new PriorityQueue<Arco>(origen.getArco().darTamaÒo(), menorTiempo);
+		while(!queueSiguientes.isEmpty()){
+			//toma el v√©rtice de ese primer arco
+			Arco actual=queueSiguientes.poll();
+			Vertice destActual= actual.getDestino();
+			//si es la meta, retorna
+			respuesta.agregar(actual);
+
+			if (destActual.equals(meta)) {
+				return respuesta;
 			}
-			return  respuesta;
+			Lista<Arco> agregar = destActual.getArco();
+			Collections.sort(agregar, menorTiempo);
+			queueSiguientes.addAll((Collection<? extends Arco>) agregar);
+
+			//http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
+		}
+		return  respuesta;
 	}
 }
--- Proyecto3/Proyecto3_v1/src/mundo/ParteA.java
+++ Proyecto3/Proyecto3_v1/src/mundo/ParteA.java
@@ -1,43 +1,108 @@
 package mundo;
 
+import java.util.Scanner;
+
+import estructuras.Arco;
+import estructuras.Dijkstra;
+import estructuras.GrafoDirigido;
+import estructuras.Lista;
+import estructuras.TablaHash;
+import estructuras.Vertice;
+
 public class ParteA {
-	
+
+	private Dijkstra d;
+	private CCT c;
+	private OutputKML ok;
+	private int i1;
+	private int i2;
 	public ParteA(){
 		/**
-         * TODO Instanciar el calculador de rutas por latitud y longitud
-         */
+		 * TODO Instanciar el calculador de rutas por latitud y longitud
+		 */
 	}
 
-    public void calcularRutaPorDistancia() {
-    	
-        long tInicioCalculoRuta = System.nanoTime();
-        /**TODO Requerimiento 1: Encontrar la ruta más corta en distancia entre dos centros de acopio 
-         * (destino y origen) dadas: 
-         * la latitud y longitud del punto de origen 
-         * radio de un espacio circular para el punto de origen (km)
-         * la latitud y longitud del punto de destino
-         * radio de un espacio circular para el punto de destino (km)
-         * Se obtiene un objeto iterable que contenga los arcos que integran la ruta mas corta para los
-         * puntos dados
-         */
-         long tTotalConsultaClientes = System.nanoTime() - tInicioCalculoRuta; 
-        System.out.println("tiempo Consulta Clientes: " + tTotalConsultaClientes + " nseg");
+	public void calcularRutaPorDistancia() {
+
+		long tInicioCalculoRuta = System.nanoTime();
+		/**TODO Requerimiento 1: Encontrar la ruta más corta en distancia entre dos centros de acopio 
+		 * (destino y origen) dadas: 
+		 * la latitud y longitud del punto de origen 
+		 * radio de un espacio circular para el punto de origen (km)
+		 * la latitud y longitud del punto de destino
+		 * radio de un espacio circular para el punto de destino (km)
+		 * Se obtiene un objeto iterable que contenga los arcos que integran la ruta mas corta para los
+		 * puntos dados
+		 */
+		//NO SE TOMA EN CUENTA LA DISTANCIA RADIAL
+		String respuesta = "";
+		System.out.println("Inserte calle y carrera del origen");
+		Scanner in = new Scanner(System.in);
+		String orig = in.nextLine();
+		GrafoDirigido gd = c.devolverGrafo();
+		TablaHash n = gd.devolverNombres();
+		System.out.println("Inserte calle y carrera del destino");
+		String dest = in.nextLine();
+		String m = in.nextLine();
+		String s1 = (String) n.buscarPorValor(orig);
+		if(s1==null)
+		{
+			respuesta = "Verifique la direccion del origen e intente de nuevo";
+		}
+		String s2 = (String) n.buscarPorValor(dest);
+		if(s2==null)
+		{
+			respuesta = "Verifique la direccion del destino e intente de nuevo";
+		}
+		i1 = Integer.valueOf(s1);
+		i2 = Integer.valueOf(s2);
+		Vertice origen = (Vertice) gd.obtenerVertice(i1);
+		Vertice destino = (Vertice) gd.obtenerVertice(i2);
+		Lista r = d.dijkstraTiempo(origen, destino);
+		Arco[] o = (Arco[]) r.darElementos();
+		double tiempo = 0;
+		for(int i = 0; i < o.length; i++)
+		{
+			Arco a = o[i];
+			tiempo += (double)a.getValor()/(double)a.getValor2();
+			Vertice v = a.getOrigen();
+			System.out.println(v);
+		}
+		if(tiempo > 0)
+		{
+			respuesta = "El tiempo de viaje es de: " + tiempo;
+		}
+		else
+		{
+			try 
+			{
+				throw new Exception("No se pudo hallar la ruta");
+			} 
+			catch (Exception e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		long tTotalConsultaClientes = System.nanoTime() - tInicioCalculoRuta; 
+		System.out.println("tiempo Consulta Clientes: " + tTotalConsultaClientes + " nseg");
 
 
-        /**TODO Mostrar la distancia de la ruta más corta o retornar una excepción en caso de no poder encontrarlo */
+		/**TODO Mostrar la distancia de la ruta más corta o retornar una excepción en caso de no poder encontrarlo */
 
-    }
-    
-    public void exportarUltimaRutaCalculada(){
-    	long tInicioExportar = System.nanoTime();
-        /**TODO Requerimiento 2: Exportar en un archivo de tipo JSON/KML de la respuesta del 
-         * requerimiento R1 (en caso de existir) para poderse mostrar en Google Maps
-        * Recibe un mensaje de éxito o de error
-         */
-        long tTotalRuta = System.nanoTime() - tInicioExportar; 
-        System.out.println("tiempo creacion ruta: " + tTotalRuta + " nseg");
+	}
+
+	public void exportarUltimaRutaCalculada(){
+		long tInicioExportar = System.nanoTime();
+		/**TODO Requerimiento 2: Exportar en un archivo de tipo JSON/KML de la respuesta del 
+		 * requerimiento R1 (en caso de existir) para poderse mostrar en Google Maps
+		 * Recibe un mensaje de éxito o de error
+		 */
+		ok.getShortestPath(i1, i2);
+		long tTotalRuta = System.nanoTime() - tInicioExportar; 
+		System.out.println("tiempo creacion ruta: " + tTotalRuta + " nseg");
 
 
-        /**TODO Mostrar el resultado de la exportación */
-    }
+		/**TODO Mostrar el resultado de la exportación */
+	}
 }
